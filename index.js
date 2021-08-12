252cbe0bfe4c62e8e28ce3604b4fc7f2a6854c0d
Zhdanov Valery <becwal@yandex.ru>
2021-08-12 13:23:56 +0300

    Создание простого http сервера

--- index.js
+++ index.js
@@ -1,10 +1,36 @@
 const http = require('http')
 
 const server = http.createServer((req, res)=>{
- //console.log(req.url)
-  res.write('<h1>hello fron Node JS</h1>')
-  res.write('<h3>hello fron Node JS</h3>')
-  res.end()
+  if(req.method==='GET')
+  {
+    res.writeHead(200,{
+      'Content-type':'text/html'
+    })
+    res.end('<form method="POST" action="/" >'+
+    '   <input type="text" name="title" /> '+
+      '<button type="submit">SEND</button>   </form>'
+
+)
+ 
+
+  }
+  else if(req.method==="POST")
+  {
+    let body =[]
+    res.writeHead(
+      200,{'Content-type':'text/html; charset=utf-8'
+      })
+    req.on('data',data=>{
+      body.push(Buffer.from(data))
+    })
+    req.on('end',()=>{
+    
+     const message=body.toString().split('=')[1]
+     res.end(`<h2>Ваше сообщение: ${message}</h2>`)
+    })
+    
+  }
+
 })
 
 server.listen(3000, ()=>{
