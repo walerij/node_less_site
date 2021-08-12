ad978ff087a65406ebec532252bffd5aeb39c41d
Zhdanov Valery <becwal@yandex.ru>
2021-08-12 12:42:20 +0300

    скрипты для node и nodemon

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const http = require('http')
 const server = http.createServer((req, res)=>{
  //console.log(req.url)
   res.write('<h1>hello fron Node JS</h1>')
-  res.write('<h2>hello fron Node JS</h2>')
+  res.write('<h3>hello fron Node JS</h3>')
   res.end()
 })
 
